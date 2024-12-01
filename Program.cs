35df848c6d6bcb602149667f3d3e7ded08be119e
Benjamin <petebs10@gmail.com>
2024-12-01 11:05:36 -0600

    day 1 part 1 solve

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Advent2024.Solutions;
+using System.Runtime.CompilerServices;
+
+string day1Input = File.ReadAllText($"{Path.GetDirectoryName(GetThisFilePath())}\\Inputs\\Day1Input.txt");
+
+Console.WriteLine("Day 1 part 1 solution: " + Day1Part1.Solve(day1Input));
+
+
+
+
+static string GetThisFilePath([CallerFilePath] string path = null)
+{
+    return path;
+}
---
+++ Solutions/Day1Part1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2024.Solutions;
+
+public class Day1Part1
+{
+    public static int Solve(string input)
+    {
+        List<string> lines = input.Split(Environment.NewLine).ToList();
+
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+
+        foreach (string line in lines)
+        {
+            List<string> splitLine = line.Split("   ").ToList();
+
+            list1.Add(int.Parse(splitLine[0]));
+            list2.Add(int.Parse(splitLine[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        int distance = 0;
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            distance += Math.Abs(list1[i] - list2[i]);
+        }
+
+        return distance;
+    }
+
+}
