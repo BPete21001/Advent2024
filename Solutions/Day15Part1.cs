640dd5b5111964876b5dcae52f01930116c6e240
Benjamin Petersen <petebs10@gmail.com>
2024-12-15 14:31:26 -0600

    frame up day 15

---
+++ Solutions/Day15Part1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2024.Solutions;
+
+public class Day15Part1
+{
+    public static int Solve(string input)
+    {
+        return 1;
+    }
+
+}
